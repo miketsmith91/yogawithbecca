3a98481a9b7a3253acfe1ab94ef0d886732e386a
Michael Smith <miketsmith91@gmail.com>
2018-02-01 10:11:07 -0600

    Initial commit

---
+++ src/test/java/com/yogawithbecca/yogawithbecca/YogawithbeccaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yogawithbecca.yogawithbecca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YogawithbeccaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
